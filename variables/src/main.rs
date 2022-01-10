237556d6b1f1febbfa7bf98062777df174b4df32
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2022-01-10 21:17:44 +0900

    [feat]データ型についての学習

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -6,12 +6,68 @@
 //     println!("The value of x is: {}", x);
 // }
 
+// fn main() {
+//     let x = 5;
+//
+//     let x = x + 1;
+//
+//     let x = x * 2;
+//
+//     println!("The value of x is: {}", x);
+// }
+
+// fn main() {
+//     let x = 2.0; // f64
+//
+//     let y: f32 = 3.0; // f32
+// }
+
+// fn main() {
+//     let sum = 5 + 10;
+//
+//     let difference = 95.5 - 4.3;
+//
+//     let product = 4 * 30;
+//
+//     let quotient = 56.7 / 32.2;
+//
+//     let remainder = 43 % 5;
+// }
+
+// fn main() {
+//     let t = true;
+//
+//     let f: bool = false;
+// }
+
+// fn main() {
+//     let c = 'z';
+//     let z = 'Z';
+//     let heart_eyed_cat = '😻';
+// }
+
+// fn main() {
+//     let tup: (i32, f64, u8) = (500, 6.4, 1);
+//
+//     let (x, y, z) = tup;
+//
+//     println!("The value of y is :{}", y);
+//
+//     let five_hundred = tup.0;
+//
+//     let six_point_for = tup.1;
+//
+//     let one = tup.2;
+// }
+
 fn main() {
-    let x = 5;
+    let a = [1, 2, 3, 4, 5];
 
-    let x = x + 1;
+    let first = a[0];
+    let second = a[1];
 
-    let x = x * 2;
+    let index = 10;
+    let element = a[index];
 
-    println!("The value of x is: {}", x);
+    println!("The value of element is: {}", element)
 }
