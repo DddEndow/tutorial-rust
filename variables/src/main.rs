90354e0992b69ff96704d99107aede865aa0f3cc
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2022-01-10 20:58:10 +0900

    [feat]セクションの分け方を修正

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,12 @@
-fn main() {
-    // let x = 5;
-    // let mut x = 5;
-    // println!("The value of x is: {}", x);
-    // x = 6;
-    // println!("The value of x is: {}", x);
+// fn main() {
+//     // let x = 5;
+//     let mut x = 5;
+//     println!("The value of x is: {}", x);
+//     x = 6;
+//     println!("The value of x is: {}", x);
+// }
 
+fn main() {
     let x = 5;
 
     let x = x + 1;
